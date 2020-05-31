e7ecefa360c7d2692010461365e6be01e0d3e5d8
vijaymadoori <66196957+vijaymadoori@users.noreply.github.com>
2020-05-31 12:40:03 +0530

    Update Calculator.java

--- src/main/java/mypackage/Calculator.java
+++ src/main/java/mypackage/Calculator.java
@@ -13,7 +13,7 @@ public class Calculator extends HttpServlet
 	
 	public long subFucn(long first, long second){
 		
-		return second-first;
+		return first-second;
 	}
 	
 	public long mulFucn(long first, long second){
